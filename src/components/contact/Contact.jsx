68ab39c84648586b53cfc015f1945f0f20de6fd0
Diego Fernandez <diegoafanador10@gmail.com>
2024-08-24 23:03:46 -0400

    [FIX]contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,15 +4,15 @@ import { FaGithub } from "react-icons/fa";
 import "./contact.scss"
 
 export default function Contact() {
-    const [message,setMessage] = useState(false)
+    const [message,setMessage] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        setMessage(true)
+        window.location.href = `mailto:diegoafanador10@gmail.com?subject=Contact&body=${message}`;
     }
 
     return (
-        <div>
+        <div className="container_contact">
             <div className="contact" id='contact'>
                 <div className="left">
                     <img src="assets/shake.svg" alt="" />
@@ -20,18 +20,21 @@ export default function Contact() {
                 <div className="right">
                     <h2>Contact</h2>
                     <form onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Email" />
-                        <textarea placeholder="Message"></textarea>
-                        <button type="submit" >Send</button>
-                        {message && <spam>Thanks, i'll reply :D</spam> }
+                        {/* <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/> */}
+                        <textarea placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                        <button type="submit" >Contact</button>
                     </form>
-                    <div>
-                        <FaLinkedin/>
-                        <FaGithub/>
+                    <div className="contact_links">
+                        <a href='https://github.com/diegofer03' rel="noreferrer" target="_blank">
+                            <FaLinkedin/>
+                        </a>
+                        <a href='https://www.linkedin.com/in/diego-fernandez-b27077157/' rel="noreferrer" target="_blank">
+                            <FaGithub/>
+                        </a>
                     </div>
                 </div>
             </div>
-            <p>Developed by @diegofer03</p>
+            <p >Developed by <a className="develop" href='https://github.com/diegofer03' rel="noreferrer" target="_blank">@diegofer03</a></p>
         </div>
     )
 }
